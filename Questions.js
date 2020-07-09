8971132ca3ae472439d224cffe05b0ddf4f20f1e
Sarah <soobin.sarah.hwang@gmail.com>
2020-07-09 00:30:53 -0700

    Building base of code

---
+++ Questions.js
@@ -0,0 +1,25 @@
+const questions = {
+    question: 'What method is used to locate an element by ID?'
+    choices: ['getElementbyID(), querySelector(), console.log(), lastElementChild'] 
+    answer: 'getElementbyID()'
+},
+{
+    qustion: 'Which of the following indicates that a varaible has not been declared? '
+    choices: ['varaiable does not exist, null, undefined, false']
+    answer: 'undefined'
+},
+{
+    question: 'When compring two values, which of the following will compare without converting values to a common type? '
+    choices: ['=, ==, ===, ====']
+    answer: '==='
+},
+{
+    question: 'Which of the following symbol means AND?''
+    choices: [' \\, ||, &&, ()']
+    answer: '&&'
+},
+{
+    question: 'What function is used to convert strings to numbers that allow decimals?'
+    choices: ['parseInt(), Number, parseFloat(), convertstring()']
+    answer: 'parseFloat()'
+}
